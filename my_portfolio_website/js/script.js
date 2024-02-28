d9d20a9435f7232e9abfb4104ce1cdd5a421a4c5
Coder-Avik <128571756+AvikMondal12@users.noreply.github.com>
2024-02-29 02:32:11 +0530

    Add files via upload

---
+++ my_portfolio_website/js/script.js
@@ -0,0 +1,7 @@
+// typing Animation
+var typed =new Typed(".typing",{
+    strings:[" ","Web Designer","Software Developer","Web developer"],
+    typespeed:100,
+    Backspeed:60,
+    loop:true
+})
